6225bb7e3b44df06537d1910bcb6977b3205c5bd
neo94 <baitichhamza@gmail.com>
2023-09-07 12:38:09 +0100

    js validation done

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 const password2 = document.querySelector('#password2');
 
-console.log(email.id);
-
 // show error
 const checkError = (element, text) => {
   const formInput = element.parentElement;
@@ -21,16 +19,28 @@ const checkSuccess = (element) => {
 };
 
 //validate email
-function validEmail(email) {
+function checkEmail(email) {
   const regex = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
-  return regex.test(email);
+  if (regex.test(email.value)) {
+    checkSuccess(email);
+  } else {
+    checkError(email, 'invalid email please check again');
+  }
 }
 
+//check password match
+function checkMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    checkError(input2, 'passwords do not match');
+  }
+}
 //validate fields
 const checkFields = (elementArr) => {
   elementArr.forEach((element) => {
     if (element.value === '') {
       checkError(element, `${getId(element)} cannot be empty`);
+    } else if (element.id === 'email') {
+      checkEmail(element);
     } else {
       checkSuccess(element);
     }
@@ -41,8 +51,23 @@ const checkFields = (elementArr) => {
   }
 };
 
+//check length
+const checkLength = (element, min, max) => {
+  if (element.value.length <= min) {
+    checkError(element, `${element.id} cannot be less than ${min} characters`);
+  } else if (element.value.length > max) {
+    checkError(
+      element,
+      `${element.id} cannot be longer than ${max} characters`
+    );
+  }
+};
+
 //event listeners
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   checkFields([userName, email, password, password2]);
+  checkLength(userName, 3, 15);
+  checkLength(password, 8, 20);
+  checkMatch(password, password2);
 });
